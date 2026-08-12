a7b103d155f9fe129ddd8900aa8f894341475a9d
Imanol Ortiz <imanolof29@gmail.com>
2026-08-12 16:54:40 +0200

    Initial Commit

---
+++ Conqr/ConqrApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConqrApp.swift
+//  Conqr
+//
+//  Created by Imanol Ortiz on 12/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct ConqrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
